a855254c0d5a8ae0fb37511bb45bbfaf92c1e51a
katmoormann99 <91487157+katmoormann99@users.noreply.github.com>
2021-10-01 18:03:11 -0400

    Update antoine.py
    Testing

--- antoine.py
+++ antoine.py
@@ -20,6 +20,8 @@ def antoine( a, T ):
     #
     # Code originally by: James C. Sutherland
     # Modified by: Tyler R. Josephson
+    
+    # test 1 2 3
 
     Ps = 10.0**( a[:,0] - a[:,1] / ( a[:,2] + T ) )
     return Ps
