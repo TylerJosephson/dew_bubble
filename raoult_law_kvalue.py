9c0b4c1f17b9a2173bbdde06a80c4dae354ff39b
Atieh <125846164+Atieh1@users.noreply.github.com>
2023-02-22 18:10:25 -0500

    Update raoult_law_kvalue.py
    Edited propane to be more accurate to the NIST website.

--- raoult_law_kvalue.py
+++ raoult_law_kvalue.py
@@ -48,8 +48,9 @@ if __name__ == '__main__':
     P = 101325  # Pressure in Pa
 
     # We should update this example! Units of test not consistent with NIST
-    propane = [6.80398, 803.810,  246.990]
-    benzene = [6.90565, 1211.033, 220.79 ]
+    
+    propane = [4.53678, 1149.36, 24.906] #These are for temperatures between 277.6 - 360.8 K
+    benzene = [4.72583, 1660.652, -1.461]
     antoineCoefs = np.array( [propane, benzene] )
 
     z = 0.5
